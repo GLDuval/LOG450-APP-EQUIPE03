63509cedbc4b019ff75cf963474d59aeb4e92e1e
Camille Brulotte <camille.brulotte@icloud.com>
2023-01-23 21:02:40 -0500

    Add search bar in recipes and map

--- src/screens/pages/grocery-map.tsx
+++ src/screens/pages/grocery-map.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import {Dimensions, ScrollView, StyleSheet, TouchableHighlight} from 'react-native';
+import {Dimensions, ScrollView, StyleSheet, TextInput, TouchableHighlight} from 'react-native';
 import {Assets, Colors, Icon, Text, View} from 'react-native-ui-lib';
 import {observer} from 'mobx-react';
 import {NavioScreen} from 'rn-navio';
 import { navio } from '..';
 
 export const GroceryMap: NavioScreen = observer(({}) => {
+  const onChangeText = (text: string) => {
+    let message = '';
+    if (text === '') {
+      message = 'This field is mandatory';
+    }
+    if (text === 'Zzz') {
+      message = 'Please enter a valid text';
+    }
+    setState({error: message});
+  };
+
   // STYLES
   const styles = StyleSheet.create({
     page: {
@@ -17,13 +28,14 @@ export const GroceryMap: NavioScreen = observer(({}) => {
       fontSize: 24,
       flex: 1,
       paddingTop: 60,
-      paddingBottom: 30,
+      paddingBottom: 20,
       marginHorizontal: 20,
     },
     title: {
       fontSize: 24,
       flex: 1,
       color: Colors.white,
+      paddingBottom: 15,
     },
     text: {
       fontSize: 20,
@@ -32,15 +44,22 @@ export const GroceryMap: NavioScreen = observer(({}) => {
     backIcon: {
       marginTop: 8,
       tintColor: Colors.white,
-    }
+    },
+    textInput: {
+      fontSize: 16,
+      padding: 10,
+      backgroundColor: '#DADADA',
+      borderRadius: 10,
+      color: '#7E7E7E',
+    },
   });
   
   return (
     <View flex style={{ backgroundColor: '#578699'}}>
       <ScrollView contentInsetAdjustmentBehavior="always">
         <View style={styles.topContainer}>
-            <View style={{flexDirection:"row"}}>
-              <TouchableHighlight
+          <View style={{flexDirection:"row"}}>
+            <TouchableHighlight
               underlayColor="Colors.transparent"
                 onPress={() => {
                   navio.pop();
@@ -52,10 +71,17 @@ export const GroceryMap: NavioScreen = observer(({}) => {
                   style={styles.backIcon}
                   />
               </TouchableHighlight>
-                <Text style={styles.title} center>
-                    Carte
-                </Text>
-            </View>
+            <Text style={styles.title} center>
+              Carte
+            </Text>
+          </View>
+          <View style={{padding:10}}>
+            <TextInput
+              placeholder="Rechercher"
+              onChangeText={() => onChangeText}
+              style={styles.textInput}
+            />
+          </View>
         </View>
 
         <View style={styles.page} bg-bgColor>
@@ -67,3 +93,7 @@ export const GroceryMap: NavioScreen = observer(({}) => {
     </View>
   );
 });
+function setState(arg0: { error: string; }) {
+  throw new Error('Function not implemented.');
+}
+
