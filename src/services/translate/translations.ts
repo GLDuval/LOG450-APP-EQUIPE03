d3bb53644fa82561cdd2da9a9292c8767b6eaa93
Camille Brulotte <camille.brulotte@icloud.com>
2023-01-24 14:11:45 -0500

    Add i18n

--- src/services/translate/translations.ts
+++ src/services/translate/translations.ts
@@ -3,7 +3,35 @@ export const en = {
     title: "Welcome back",
     email: "Email address",
     password: "Password",
-  }
+  },
+  dashboard: {
+    title: "Dashboard",
+    hello: "Hello,",
+    myRecipes: "My Recipes",
+    groceryList: "Grocery List",
+    map: "Map",
+    groceries: "Groceries",
+  },
+  myRecipes: {
+    title: "My Recipes",
+  },
+  groceryList: {
+    title: "Grocery List",
+  },
+  groceryInfos: {
+    flyer: "Flyer",
+    recipes: "Recipes",
+  },
+  map: {
+    title: "Map",
+  },
+  profile: {
+    title: "Profile",
+    chooseLanguage: "Choose your language",
+  },
+  actions: {
+    logout: "Logout",
+  },
 };
 
 export const fr = {
@@ -11,5 +39,33 @@ export const fr = {
     title: "Content de te revoir",
     email: "Courriel",
     password: "Mot de passe",
-  }
+  },
+  dashboard: {
+    title: "Tableau de bord",
+    hello: "Bonjour,",
+    myRecipes: "Mes recettes",
+    groceryList: "Liste d'épiceries",
+    map: "Carte",
+    groceries: "Épicerie",
+  },
+  myRecipes: {
+    title: "Mes recettes",
+  },
+  groceryList: {
+    title: "Liste d'épiceries",
+  },
+  map: {
+    title: "Carte",
+  },
+  groceryInfos: {
+    flyer: "Circulaire",
+    recipes: "Recettes",
+  },
+  profile: {
+    title: "Profil",
+    chooseLanguage: "Choisissez votre langue",
+  },
+  actions: {
+    logout: "Se déconnecter",
+  },
 };
