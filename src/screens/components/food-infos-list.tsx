8c098dcc5bf01ab064d76bcc7694bef354cc2541
JonathanDuvalV <duvalvenne.jonathan@gmail.com>
2023-02-01 09:23:31 -0500

    Connected firestore + infinite scroll

--- src/screens/components/food-infos-list.tsx
+++ src/screens/components/food-infos-list.tsx
@@ -1,69 +1,36 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { StyleSheet, Image, TextInput, FlatList } from 'react-native';
 import { Text, View } from 'react-native-ui-lib';
 import { observer } from 'mobx-react';
 import { NavioScreen } from 'rn-navio';
 import { getTheme } from '../../utils/designSystem';
+import { getProducts, getProductsNextBatch } from '../../services/firestoreService';
+import { Product } from '../../models/product';
 
 export const FoodInfosList: NavioScreen = observer(() => {
-  const food = [
-    {
-      product_name: 'Mini concombres',
-      image_url: 'https://product-images.metro.ca/images/h97/h75/11204994465822.jpg',
-      regular_price: '2,99 $ ch.',
-      sale_price: '3,99 $ ch.',
-      quantity: 2,
-    },
-    {
-      product_name: 'Tomates rouges en grappe',
-      image_url: 'https://product-images.metro.ca/images/h09/h62/9276941893662.jpg',
-      regular_price: '5,11 $ env. ch. (775 g env.)',
-      sale_price: '8,80 $ env. kg',
-      quantity: 4,
-    },
-    {
-      product_name: 'Bœuf haché mi-maigre',
-      image_url: 'https://product-images.metro.ca/images/h5b/hcb/9225276358686.jpg',
-      regular_price: '3,96 $ / 450 g',
-      sale_price: '13,87 $ / kg',
-      quantity: 1,
-    },
-    {
-      product_name: 'Pistaches rôties et salées',
-      image_url: 'https://product-images.metro.ca/images/h97/h8c/9633873330206.jpg',
-      regular_price: '4,99 $  ch.',
-      sale_price: '5,99 $  ch.',
-      quantity: 1,
-    },
-    {
-      product_name: 'Macaroni et fromage original',
-      image_url: 'https://product-images.metro.ca/images/h29/h94/9986997256222.jpg',
-      regular_price: '4 /  5,00 $',
-      sale_price: '1,59 $  ch.',
-      quantity: 1,
-    },
-    {
-      product_name: 'Fromage à la crème original',
-      image_url: 'https://product-images.metro.ca/images/hde/h06/9986804482078.jpg',
-      regular_price: '4,77 $  ch.',
-      sale_price: '5,99 $  ch.',
-      quantity: 1,
-    },
-    {
-      product_name: 'Macaroni et fromage original',
-      image_url: 'https://product-images.metro.ca/images/h29/h94/9986997256222.jpg',
-      regular_price: '4 /  5,00 $',
-      sale_price: '1,59 $  ch.',
-      quantity: 1,
-    },
-    {
-      product_name: 'Fromage à la crème original',
-      image_url: 'https://product-images.metro.ca/images/hde/h06/9986804482078.jpg',
-      regular_price: '4,77 $  ch.',
-      sale_price: '5,99 $  ch.',
-      quantity: 1,
-    },
-  ];
+  const [product, setProduct] = React.useState<Product[]>([]);
+
+  useEffect(() => {
+    getProducts().then(
+      (products) => {
+        setProduct(products);
+      },
+      (err) => {
+        console.log(err);
+      },
+    );
+  }, []);
+
+  const fetchNextBatch = () => {
+    const lastProduct = product[product.length - 1];
+    getProductsNextBatch(lastProduct).then(
+      (nextBatch) => {
+        setProduct([...product, ...nextBatch]);
+      },
+      (err) => console.log(err),
+    );
+  };
 
   // STYLES
   const styles = StyleSheet.create({
@@ -109,7 +76,9 @@ export const FoodInfosList: NavioScreen = observer(() => {
 
   return (
     <FlatList
-      data={food}
+      onEndReached={fetchNextBatch}
+      onEndReachedThreshold={0.5}
+      data={product}
       renderItem={({ item }) => (
         <View style={styles.cardContainer}>
           <View style={styles.card}>
