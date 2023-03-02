7326717fda8f1980ee74a4cceea833e25357c9cc
Jonathan Duval-Venne <duvalvenne.jonathan@gmail.com>
2023-03-01 20:51:42 -0500

    Added Recipes query

--- src/services/firestoreService.tsx
+++ src/services/firestoreService.tsx
@@ -1,6 +1,7 @@
 import { db } from '../../firebaseConfig';
 import { collection, getDocs, query, orderBy, limit, startAfter } from 'firebase/firestore';
 import { Product } from '../models/Product';
+import { Recipe } from '../models/Recipe';
 
 export const getProducts = async () => {
   const products: Product[] = [];
@@ -33,4 +34,16 @@ export const getProductsNextBatch = async (lastProduct: Product) => {
   });
 
   return products;
+};
+
+export const getRecipes = async () => {
+  const recipes: Recipe[] = [];
+
+  const firestoreQuery = query(collection(db, 'recipes'));
+  const querySnapshot = await getDocs(firestoreQuery);
+  querySnapshot.forEach((doc) => {
+    recipes.push(doc.data() as Recipe);
+  });
+
+  return recipes;
 };
